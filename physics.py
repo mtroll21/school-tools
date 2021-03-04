58bbc81b94e1a0097aad5b404d0a5ebff631baad
notmatej <matejpech123@gmail.com>
2021-03-04 11:38:05 +0100

    Create physics.py

---
+++ physics.py
@@ -0,0 +1,14 @@
+#physics operation you want to do
+while True:
+    operation = input("test")
+    if operation == "a":
+        print ("yes")
+    if operation == "b":
+        print ("no")
+    if operation == "c":
+        print ("yes´nt")
+
+    
+    #stop the lop
+    if operation == "stop":
+        break
